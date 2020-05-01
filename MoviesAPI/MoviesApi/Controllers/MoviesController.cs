01dd2f2974af3bd552cd629a61387ec1cc64cd77
Stenli <stanil_dimitrov@mail.bg>
2020-05-01 08:21:50 +0300

    Add documentation in movie controller.

--- MoviesAPI/MoviesApi/Controllers/MoviesController.cs
+++ MoviesAPI/MoviesApi/Controllers/MoviesController.cs
@@ -12,29 +12,51 @@ using System.Threading.Tasks;
 
 namespace MoviesApi.Controllers
 {
+    /// <summary>
+    /// A class that redirects user's request to specific movie action.
+    /// It inherits class ControllerBase.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
         private readonly IMovieStore _movieStore;
-        private readonly ILogger<MoviesController> _logger;
         private readonly IMemoryCache _cache;
+        private readonly ILogger _logger;
 
+        /// <summary>
+        /// Constructor for MoviesController
+        /// </summary>
+        /// <param name="movieStore">Provides access to CRUD operations on SecurityProfile</param>                
+        /// <param name="cache">Provides access to use memory cache</param>                
+        /// <param name="logger">Provides logging services</param> 
         public MoviesController(IMovieStore movieStore, ILogger<MoviesController> logger, IMemoryCache cache )
         {
             _movieStore = movieStore ?? throw new ArgumentNullException(nameof(movieStore));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// Get list of movies action
+        /// </summary>
+        /// <param name="request">The request model</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled</param>
+        /// <returns>Query result instance containing total count, total page count and the result set.</returns>
         // GET: api/Movies
         [HttpGet]
-        public async Task<ActionResult<QueryResult<MovieGridResponseModel>>> GetMovieGridAsync(BasicQuery query, CancellationToken cancellationToken)
+        public async Task<ActionResult<QueryResult<MovieGridResponseModel>>> GetMovieGridAsync(BasicQuery request, CancellationToken cancellationToken)
         { 
             _logger.LogInformation("Call made to GetMovieGrid.");
-            return await _movieStore.GetMovieGridAsync(query, cancellationToken);
+            return await _movieStore.GetMovieGridAsync(request, cancellationToken);
         }
 
+        /// <summary>
+        /// Get movie details action
+        /// </summary>
+        /// <param name="id">The movie unique identifier</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled</param>
+        /// <returns>Movie details response model</returns>
         // GET: api/Movies/5
         [HttpGet("{id}")]
         public async Task<ActionResult<MovieDetailsResponseModel>> GetMovieDetailsAsync(int id, CancellationToken cancellationToken)
@@ -57,18 +79,26 @@ namespace MoviesApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Create movie action
+        /// </summary>
+        /// <param name="MovieCreateRequestModel">Input parameters for creation of movie wrapped in an object</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled</param>
+        /// <returns>The new movie unique identifier</returns>
         // POST: api/Movies
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<int>> CreateMovieAsync(MovieCreateRequestModel request, CancellationToken cancellationToken)
         {
             _logger.LogInformation("Call made to CreateMovieAsync.");
-            var response = await _movieStore.CreateMovieAsync(request, cancellationToken);
-
-            return response;
+            return await _movieStore.CreateMovieAsync(request, cancellationToken);
         }
 
+        /// <summary>
+        /// Update movie action
+        /// </summary>
+        /// <param name="MovieUpdateRequestModel">Input parameters for updating of movie profile wrapped in an object</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled</param>
+        /// /// <returns>The task</returns>
         // PUT: api/Movies/5
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateMovieAsync(int id, MovieUpdateRequestModel request, CancellationToken cancellationToken)
@@ -77,6 +107,7 @@ namespace MoviesApi.Controllers
             try
             {
                 await _movieStore.UpdateMovieAsync(id, request, cancellationToken);
+                return Ok();
             }
             catch (NotFoundException ex)
             {
@@ -88,10 +119,14 @@ namespace MoviesApi.Controllers
                 _logger.LogError("Unexpected exception occured:", ex);
                 throw;
             }
-
-            return Ok();
         }
 
+        /// <summary>
+        /// Delete movie action
+        /// </summary>
+        /// <param name="id">The movie unique identifier</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled</param>
+        /// /// <returns>The task</returns>
         // DELETE: api/Movies/5
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteMovieAsync(int id, CancellationToken cancellationToken)
@@ -100,6 +135,7 @@ namespace MoviesApi.Controllers
             try
             {
                 await _movieStore.DeleteMovieAsync(id, cancellationToken);
+                return Ok();
 
             }
             catch (NotFoundException ex)
@@ -112,8 +148,6 @@ namespace MoviesApi.Controllers
                 _logger.LogError("Unexpected exception occured:", ex);
                 throw;
             }
-
-            return Ok();
         }
 
         private async Task<MovieDetailsResponseModel> CacheDetailsResponse(int id, CancellationToken cancellationToken)
