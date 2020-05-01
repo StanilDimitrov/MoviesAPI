37ad78c32798905bc0eeedd01eb3b3747019ed13
Stenli <stanil_dimitrov@mail.bg>
2020-05-01 15:52:54 +0300

    Remove usings

--- MoviesAPI/MoviesApi/Controllers/MoviesController.cs
+++ MoviesAPI/MoviesApi/Controllers/MoviesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using MoviesApi.Dal.Contracts;
