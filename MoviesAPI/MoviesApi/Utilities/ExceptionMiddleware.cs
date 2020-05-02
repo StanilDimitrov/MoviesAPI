ed2d55780620bb7d1eff0ed6d737026d4dd35df4
Stenli <stanil_dimitrov@mail.bg>
2020-05-02 20:39:49 +0300

    Improve middleware

--- MoviesAPI/MoviesApi/Utilities/ExceptionMiddleware.cs
+++ MoviesAPI/MoviesApi/Utilities/ExceptionMiddleware.cs
@@ -30,9 +30,15 @@ namespace MoviesApi.Utilities
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 await context.Response.WriteAsync(ex.Message);
             }
+            catch(OperationCanceledException)
+            {
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync("Your submission was canceled.");
+            }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong: {ex}");
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
                 await context.Response.WriteAsync("Something went horribly wrong!");
             }
